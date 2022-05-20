bdb35ac9da5e522cae030535dd0d7028b9cf2d61
Avery Bobbitt <avery@bobbitt.dev>
2022-05-19 21:14:09 -0400

    fixed context issue in game constructor

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// Driver
 void main() => runApp(const App());
 
+// App
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
@@ -22,6 +24,7 @@ class App extends StatelessWidget {
   }
 }
 
+// Home
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   final String title = "games";
@@ -34,30 +37,68 @@ class Home extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            _game("wordle", Wordle()),
-            _game("dordle", Dordle()),
-            _game("quordle", Quordle())
+            _game(context, "wordle", Wordle()),
+            _game(context, "dordle", Dordle()),
+            _game(context, "quordle", Quordle())
           ],
         ));
   }
-
-  ListTile _game(String title, Widget newGame) {
-    return ListTile(
-        leading: Icon(Icons.chevron_right),
-        title: Text(title,
-            style:
-                const TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => newGame),
-          );
-        });
-  }
 }
 
+// Game tile (list entry)
+ListTile _game(BuildContext context, String title, Widget newGame) {
+  // this context workaround is shit
+  return ListTile(
+      leading: Icon(Icons.chevron_right),
+      title: Text(title,
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 20)),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => newGame),
+        );
+      });
+}
+
+// Wordle
 class Wordle extends StatelessWidget {
   const Wordle({Key? key}) : super(key: key);
   final String title = "wordle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
+      ),
+    );
+  }
+}
+
+// Dordle
+class Dordle extends StatelessWidget {
+  const Dordle({Key? key}) : super(key: key);
+  final String title = "dordle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
+      ),
+    );
+  }
+}
+
+// Quordle
+class Quordle extends StatelessWidget {
+  const Quordle({Key? key}) : super(key: key);
+  final String title = "quordle";
 
   @override
   Widget build(BuildContext context) {
