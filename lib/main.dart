f08d68ac27fb3e79d48f48902ea75a76e606c91d
Avery Bobbitt <avery@bobbitt.dev>
2022-05-20 23:49:43 -0400

    added all wordle clones and included TODO comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:js';
-
+import 'package:js/js.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -9,6 +8,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 void main() => runApp(const App());
 
 // App
+// TODO: WebView doesn't support JS(?) thus breaking games
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
@@ -34,19 +34,74 @@ class Home extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
+          leading: IconButton(
+            icon: Icon(Icons.stacked_bar_chart),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => Statistics()),
+              );
+            },
+          ),
         ),
-        body: ListView(
-          children: [
-            _game(context, "wordle", Wordle()),
-            _game(context, "dordle", Dordle()),
-            _game(context, "quordle", Quordle())
-          ],
-        ));
+        body: ListView(children: [
+          game(context, "TEST", TestGame()),
+          game(context, "wordle", Wordle()),
+          game(context, "dordle", Dordle()),
+          game(context, "quordle", Quordle()),
+          game(context, "octordle", Octordle()),
+          game(context, "sedecordle", Sedecordle()),
+          game(context, "lewdle", Lewdle()),
+          game(context, "heardle", Heardle()),
+          game(context, "squirdle", Squirdle()),
+          game(context, "nerdle", Nerdle()),
+          game(context, "drugdle", Drugdle()),
+          game(context, "sweardle", Sweardle()),
+          game(context, "worldle", Worldle()),
+          game(context, "globle", Globle()),
+          game(context, "hogwartle", Hogwartle())
+        ]));
+  }
+}
+
+class Statistics extends StatelessWidget {
+  const Statistics({Key? key}) : super(key: key);
+  final String title = "Player Statistics";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // TODO: implement stats
+      body: Center(
+        child: Text.rich(
+          TextSpan(
+            children: [
+              WidgetSpan(
+                  child: Icon(
+                Icons.warning_rounded,
+                color: Colors.yellow,
+              )),
+              TextSpan(text: " work in progress "),
+              WidgetSpan(
+                  child: Icon(
+                Icons.warning_rounded,
+                color: Colors.yellow,
+              )),
+            ],
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
   }
 }
 
 // Game tile (list entry)
-ListTile _game(BuildContext context, String title, Widget newGame) {
+// TODO: define generic game constructor here (ret: StatelessWidget)
+ListTile game(BuildContext context, String title, Widget newGame) {
   // this context workaround is shit
   return ListTile(
       leading: Icon(Icons.chevron_right),
@@ -57,6 +112,24 @@ ListTile _game(BuildContext context, String title, Widget newGame) {
           MaterialPageRoute(builder: (context) => newGame),
         );
       });
+}
+
+// TEST GAME
+class TestGame extends StatelessWidget {
+  const TestGame({Key? key}) : super(key: key);
+  final String title = "test game";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://bobbitt.dev',
+      ),
+    );
+  }
 }
 
 // Wordle
@@ -71,7 +144,7 @@ class Wordle extends StatelessWidget {
         title: Text(title),
       ),
       body: const WebView(
-        initialUrl: 'https://flutter.dev',
+        initialUrl: 'https://www.nytimes.com/games/wordle',
       ),
     );
   }
@@ -89,7 +162,7 @@ class Dordle extends StatelessWidget {
         title: Text(title),
       ),
       body: const WebView(
-        initialUrl: 'https://flutter.dev',
+        initialUrl: 'https://zaratustra.itch.io/dordle',
       ),
     );
   }
@@ -107,7 +180,205 @@ class Quordle extends StatelessWidget {
         title: Text(title),
       ),
       body: const WebView(
-        initialUrl: 'https://flutter.dev',
+        initialUrl: 'https://www.quordle.com/#/',
+      ),
+    );
+  }
+}
+
+// Octordle
+class Octordle extends StatelessWidget {
+  const Octordle({Key? key}) : super(key: key);
+  final String title = "octordle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://octordle.com/',
+      ),
+    );
+  }
+}
+
+// Sedecordle
+class Sedecordle extends StatelessWidget {
+  const Sedecordle({Key? key}) : super(key: key);
+  final String title = "sedecordle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.sedecordle.com/',
+      ),
+    );
+  }
+}
+
+// Lewdle
+class Lewdle extends StatelessWidget {
+  const Lewdle({Key? key}) : super(key: key);
+  final String title = "lewdle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.lewdlegame.com/',
+      ),
+    );
+  }
+}
+
+// Heardle
+class Heardle extends StatelessWidget {
+  const Heardle({Key? key}) : super(key: key);
+  final String title = "heardle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.heardle.app/',
+      ),
+    );
+  }
+}
+
+// Squirdle
+class Squirdle extends StatelessWidget {
+  const Squirdle({Key? key}) : super(key: key);
+  final String title = "squirdle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://squirdle.fireblend.com/',
+      ),
+    );
+  }
+}
+
+// Nerdle
+class Nerdle extends StatelessWidget {
+  const Nerdle({Key? key}) : super(key: key);
+  final String title = "nerdle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://nerdlegame.com/',
+      ),
+    );
+  }
+}
+
+// Drugdle
+class Drugdle extends StatelessWidget {
+  const Drugdle({Key? key}) : super(key: key);
+  final String title = "drugdle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.drugdle.com/',
+      ),
+    );
+  }
+}
+
+// Sweardle
+class Sweardle extends StatelessWidget {
+  const Sweardle({Key? key}) : super(key: key);
+  final String title = "sordle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://sweardle.com/',
+      ),
+    );
+  }
+}
+
+// Worldle
+class Worldle extends StatelessWidget {
+  const Worldle({Key? key}) : super(key: key);
+  final String title = "worldle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://worldle.teuteuf.fr/',
+      ),
+    );
+  }
+}
+
+// Globle
+class Globle extends StatelessWidget {
+  const Globle({Key? key}) : super(key: key);
+  final String title = "globle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://globle-game.com/game',
+      ),
+    );
+  }
+}
+
+// Hogwartle
+class Hogwartle extends StatelessWidget {
+  const Hogwartle({Key? key}) : super(key: key);
+  final String title = "hogwartle";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.hogwartsishere.com/hogwartle/',
       ),
     );
   }
